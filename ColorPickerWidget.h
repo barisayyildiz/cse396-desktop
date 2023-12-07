fa28ea90c5c58d77b91c353bf54a2e302d074c14
barisayyildiz <99ayyldzbaris99@gmail.com>
2023-12-07 19:59:02 +0300

    feat: color picker eklendi

--- ColorPickerWidget.h
+++ ColorPickerWidget.h
@@ -2,6 +2,7 @@
 #define COLORPICKERWIDGET_H
 
 #include <QtWidgets>
+#include <QColorDialog>
 
 class ColorPickerWidget : public QWidget {
     Q_OBJECT
@@ -10,31 +11,23 @@ public:
     ColorPickerWidget(QWidget* parent = nullptr) : QWidget(parent)
     {
         QGridLayout* layout = new QGridLayout(this);
+        color = QColor(170, 170, 170);
 
-        // Define a list of colors to fill the grid.
-        QList<QColor> colors = {
-            QColor("red"), QColor("green"), QColor("blue"), QColor("yellow"), QColor("purple"),
-            QColor("orange"), QColor("brown"), QColor("grey"), QColor("pink"), QColor("lime"),
-            QColor("cyan"), QColor("magenta"), QColor("gold"), QColor("salmon"), QColor("navy")
-        };
+        QPushButton *colorButton = new QPushButton("Pick a Color");
+        connect(colorButton, &QPushButton::clicked, this, &ColorPickerWidget::openColorDialog);
+        layout->addWidget(colorButton);
+    }
 
-        // Create buttons for colors in a 5 columns x 3 rows grid.
-        for (int i = 0; i < colors.size(); ++i) {
-            QPushButton* button = new QPushButton;
-            button->setFixedSize(20, 20); // Set fixed size for buttons
-
-            // Set the button background color and remove the border
-            QString css = QString("background-color: %1; border: none;").arg(colors[i].name());
-            button->setStyleSheet(css);
-
-            // Connect the button's clicked signal to the ColorPickerWidget's colorSelected slot
-            connect(button, &QPushButton::clicked, this, [this, colors, i]() {
-                emit colorSelected(colors[i]);
-                });
-
-            layout->addWidget(button, i / 5, i % 5); // Add to grid layout
+private slots:
+    void openColorDialog() {
+        color = QColorDialog::getColor(color, this, QString(), QColorDialog::DontUseNativeDialog);
+        if(color.isValid()) {
+            emit colorSelected(color);
         }
     }
+
+private:
+    QColor color;
 
 signals:
     void colorSelected(const QColor& color);
